45e50df8b15a3a67bf59f46e3b77517e413f8e4e
Livingstone <oklivinstone@gmail.com>
2023-02-20 06:54:57 -0800

    what betty is doing for me

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of varous types on computer it compiled and run on
- * Return: 0 (Success)
+* main - A program that prints the size of various types on the computer
+* Return: 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of a char: %ld byte(s)\n"
+"Size of an int: %ld byte(s)\n"
+"Size of a long int: %ld byte(s)\n"
+"Size of a long long int: %ld byte(s)\n"
+"Size of a float: %ld byte(s)\n",
+sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int),
+sizeof(float));
 return (0);
 }
